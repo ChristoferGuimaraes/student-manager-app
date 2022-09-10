320dc0ee1db4c6454366c9dcf689ed8f20e60d48
Christofer Guimarães <christoferguima@gmail.com>
2022-09-10 13:12:36 -0300

    table refactor

--- src/app/components/table-students/table-students.component.ts
+++ src/app/components/table-students/table-students.component.ts
@@ -14,7 +14,12 @@ export class TableStudentsComponent implements OnInit {
   
   ngOnInit(): void { 
     this.getAllStudents(); 
+    this.getHeader();
   } 
+
+  getHeader() {
+    this.service.getStudents().subscribe((data: any) => this.headers = Object.keys(data.content[0]).slice(0, 5)); 
+  }
   
   getAllStudents() { 
     this.service.getStudents().subscribe((data: any) => this.students = data.content); 
