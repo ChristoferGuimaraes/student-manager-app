34fdc0fcc33197b64ef6cc9c235b61ef822765ba
Christofer Guimarães <christoferguima@gmail.com>
2022-09-11 12:05:46 -0300

    table refactor

--- src/app/components/table-courses/table-courses.component.ts
+++ src/app/components/table-courses/table-courses.component.ts
@@ -6,22 +6,49 @@ import { PostService } from 'src/app/services/post.service';
   templateUrl: './table-courses.component.html',
   styleUrls: ['./table-courses.component.scss']
 })
-export class TableCoursesComponent implements OnInit { 
-  courses: any = []; 
-  headers: any = []; 
-  
-  constructor(private service: PostService) { } 
-  
-  ngOnInit(): void { 
-    this.getAllCourses(); 
+export class TableCoursesComponent implements OnInit {
+  courses: any = [];
+  headers: any = [];
+
+  constructor(private service: PostService) { }
+
+  ngOnInit(): void {
+    this.getAllCourses();
     this.getHeader()
-  } 
-  
-  getHeader() {
-    this.service.getCourses().subscribe((data: any) => this.headers = Object.keys(data.content[0])); 
   }
 
-  getAllCourses() { 
-    this.service.getCourses().subscribe((data: any) => this.courses = data.content); 
-  } 
+  getAllCourses(): void {
+    this.service.getCourses()
+      .subscribe((data: any) => this.courses = data.content);
+  }
+
+  getHeader() {
+    this.service.getCourses()
+      .subscribe((data: any) => {
+        this.headers = Object.keys(data.content[0])
+
+        this.formaterHeader(this.headers)
+        return this.headers;
+      });
+  }
+
+  formaterHeader(headers: any) {
+    for (let header in headers) {
+      if (headers[header] === 'name') {
+        headers[header] = 'Name';
+      }
+      if (headers[header] === 'teacherName') {
+        headers[header] = 'Teacher';
+      }
+      if (headers[header] === 'classNumber') {
+        headers[header] = 'Class';
+      }
+      if (headers[header] === 'startDate') {
+        headers[header] = 'Start Date'
+      }
+    }
+    return headers;
+  }
+
+
 }
--- src/app/components/table-students/table-students.component.ts
+++ src/app/components/table-students/table-students.component.ts
@@ -6,22 +6,49 @@ import { PostService } from 'src/app/services/post.service';
   templateUrl: './table-students.component.html',
   styleUrls: ['./table-students.component.scss']
 })
-export class TableStudentsComponent implements OnInit { 
-  students: any = []; 
-  headers: any = []; 
-  
-  constructor(private service: PostService) { } 
-  
-  ngOnInit(): void { 
-    this.getAllStudents(); 
+export class TableStudentsComponent implements OnInit {
+  students: any = [];
+  headers: any = [];
+
+  constructor(private service: PostService) { }
+
+  ngOnInit(): void {
+    this.getAllStudents();
     this.getHeader();
-  } 
+  }
 
   getHeader() {
-    this.service.getStudents().subscribe((data: any) => this.headers = Object.keys(data.content[0]).slice(0, 5)); 
+    this.service.getStudents()
+      .subscribe((data: any) => {
+        this.headers = Object.keys(data.content[0]).slice(0, 5);
+
+        this.formaterHeader(this.headers);
+        return this.headers;
+      });
   }
-  
-  getAllStudents() { 
-    this.service.getStudents().subscribe((data: any) => this.students = data.content); 
-  } 
+
+  formaterHeader(headers: any) {
+    for (let header in headers) {
+      if (headers[header] === 'id') {
+        headers[header] = 'ID';
+      }
+      if (headers[header] === 'firstName') {
+        headers[header] = 'First Name';
+      }
+      if (headers[header] === 'lastName') {
+        headers[header] = 'Last Name';
+      }
+      if (headers[header] === 'age') {
+        headers[header] = 'Age'
+      }
+      if (headers[header] === 'email') {
+        headers[header] = 'Email'
+      }
+    }
+    return headers;
+  }
+
+  getAllStudents() {
+    this.service.getStudents().subscribe((data: any) => this.students = data.content);
+  }
 }
